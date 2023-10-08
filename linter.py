9e18e8f9a6beb7d6e72724fe7a456eb8f77a2ef4
Amira Msadek <amira.msadek@insat.ucar.tn>
2023-10-08 20:00:37 +0100

    Add: create CI for project

---
+++ linter.py
@@ -0,0 +1,18 @@
+"""Python script for running static testing"""
+import subprocess
+import sys
+
+# Get the path of the environment executables from the value of the #
+# executed Python binary. Splitting differ for Windows/Linux type paths
+
+if sys.platform == "win32":
+    PATH = sys.executable.rsplit("\\", maxsplit=1)[0]
+else:
+    PATH = sys.executable.rsplit("/", maxsplit=1)[0]
+
+print("[BLACK]", flush=True)
+subprocess.run([f"{PATH}/python", "-m", "black", "--check", "."], check=False)
+
+
+print("[FLAKE 8]", flush=True)
+subprocess.run([f"{PATH}/python", "-m", "flake8", "."], check=False)
