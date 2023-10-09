479a515d39a3e33f89c444c745bed8d014e07c62
Amira Msadek <amira.msadek@insat.ucar.tn>
2023-10-09 15:25:06 +0100

    test

--- linter.py
+++ linter.py
@@ -11,8 +11,8 @@ else:
     PATH = sys.executable.rsplit("/", maxsplit=1)[0]
 
 print("[BLACK]", flush=True)
-subprocess.run([f"{PATH}/python", "-m", "black", "--check", "."], check=False)
+subprocess.run([f"{PATH}/python", "-m", "black", "--check", "."], check=True)
 
 
 print("[FLAKE 8]", flush=True)
-subprocess.run([f"{PATH}/python", "-m", "flake8", "."], check=False)
+subprocess.run([f"{PATH}/python", "-m", "flake8", "."], check=True)
